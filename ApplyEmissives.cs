b28bd25106c7ae1ec020e6768617bc367d1e3279
PSYCHO <fubartitan@gmail.com>
2020-09-19 13:27:49 +0200

    Add project files.

---
+++ ApplyEmissives.cs
@@ -0,0 +1,261 @@
+﻿using Sandbox.ModAPI;
+using System.Collections.Generic;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRageMath;
+using VRage.Utils;
+using VRage.Game;
+
+// =======================
+// PLEASE DO NOT EDIT THIS
+// =======================
+
+namespace PSYCHO.ApplyEmissives
+{
+    public class EmissiveData
+    {
+        public string EmissiveMaterialName { get; set; }
+        public Color FullyWorkingEmissiveColor { get; set; }
+        public float FullyWorkingEmissiveGlow { get; set; }
+        public Color BustedEmissiveColor { get; set; }
+        public float BustedEmissiveGlow { get; set; }
+    }
+
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+
+    class ApplyEmissivesHandler : MySessionComponentBase
+    {
+        public static ApplyEmissivesHandler EmissiveDataInstance;
+
+        public Dictionary<string, List<EmissiveData>> EmissiveUserData = new Dictionary<string, List<EmissiveData>>();
+
+        public override void LoadData()
+        {
+            EmissiveDataInstance = this;
+            ApplyEmissivesSettings.Settings settings = new ApplyEmissivesSettings.Settings();
+            settings.ConstructData();
+        }
+
+        protected override void UnloadData()
+        {
+            EmissiveDataInstance = null;
+        }
+
+        public void AddData(string subtypeId, string matName, EmissiveData data)
+        {
+            data.EmissiveMaterialName = matName;
+
+            if (!EmissiveUserData.ContainsKey(subtypeId))
+                EmissiveUserData[subtypeId] = new List<EmissiveData>();
+            EmissiveUserData[subtypeId].Add(data);
+        }
+    }
+
+    /*
+    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+
+    public class ApplySessionEmissivesLogic : MySessionComponentBase
+    {
+        ushort Tick = 0;
+
+        public override void LoadData()
+        {
+
+        }
+
+        public override void BeforeStart()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        protected override void UnloadData()
+        {
+
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+            Tick++;
+
+            if (Tick % 3 == 0)
+            {
+                if (Tick == ushort.MaxValue)
+                    Tick = 0;
+            }
+
+            RunLogic();
+        }
+
+        void RunLogic()
+        {
+
+        }
+    }
+    */
+
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_TerminalBlock), false)]
+
+    public class ApplyEmissivesLogic : MyGameLogicComponent
+    {
+        //private int Tick;
+        IMyTerminalBlock Block;
+        Vector3 BlockColor = Vector3.Zero;
+        MyStringHash BlockTexture;
+        Color EmissiveColor = new Color(0, 0, 0);
+
+        List<EmissiveData> EmissiveDataSet = new List<EmissiveData>();
+
+        ApplyEmissivesHandler EmissiveDataInstance => ApplyEmissivesHandler.EmissiveDataInstance;
+        PSYCHO.ApplyEmissivesSettings.Settings SettingsData = new ApplyEmissivesSettings.Settings();
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            if (MyAPIGateway.Utilities.IsDedicated)
+                return;
+
+            Block = Entity as IMyTerminalBlock;
+
+            /*
+            if (!MyUserData.ThrusterSubtypeIDs.Contains(block.BlockDefinition.SubtypeId))
+            {
+                Block = null;
+                return;
+            }
+            */
+
+            NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            if (Block == null)
+                return;
+
+            var subtypeId = Block.BlockDefinition.SubtypeId;
+
+            if (EmissiveDataInstance.EmissiveUserData.ContainsKey(subtypeId))
+            {
+                EmissiveDataSet = EmissiveDataInstance.EmissiveUserData[subtypeId];
+            }
+            else
+            {
+                return;
+            }
+
+            // Hook to events.
+            Block.PropertiesChanged += PropertiesChanged;
+            Block.IsWorkingChanged += IsWorkingChanged;
+
+            NeedsUpdate = MyEntityUpdateEnum.EACH_100TH_FRAME;
+        }
+
+        private void PropertiesChanged(IMyTerminalBlock obj)
+        {
+            BlockColor = Block.SlimBlock.ColorMaskHSV;
+            BlockTexture = Block.SlimBlock.SkinSubtypeId;
+
+            CheckAndSetEmissives(true);
+        }
+
+        private void IsWorkingChanged(IMyCubeBlock obj)
+        {
+            BlockColor = Block.SlimBlock.ColorMaskHSV;
+            BlockTexture = Block.SlimBlock.SkinSubtypeId;
+
+            CheckAndSetEmissives(true);
+        }
+
+        // Cleanup.
+        public override void Close()
+        {
+            Block.PropertiesChanged -= PropertiesChanged;
+            Block.IsWorkingChanged -= IsWorkingChanged;
+
+            Block = null;
+        }
+
+        // Mostly needed due to not having a 'block recolored' event.
+        public override void UpdateAfterSimulation100()
+        {
+            if (Block == null || Block.MarkedForClose || Block.Closed)
+            {
+                NeedsUpdate = MyEntityUpdateEnum.NONE;
+                return;
+            }
+
+            // Only if we want to use it within some frame set not available natively.
+            // In this instance, it's cosmetic in nature so we don't care if the effects apply at a delay so we're handling it in a update 100 (each 100th frame).
+            /*
+            Tick++;
+
+            if (Tick % 3 == 0)
+            {
+                // Do conditionals here...
+            }
+            */
+
+            bool updateEmissives = false;
+
+            if (BlockColor != Block.SlimBlock.ColorMaskHSV)
+            {
+                BlockColor = Block.SlimBlock.ColorMaskHSV;
+                updateEmissives = true;
+            }
+
+            if (BlockTexture != Block.SlimBlock.SkinSubtypeId)
+            {
+                BlockTexture = Block.SlimBlock.SkinSubtypeId;
+                updateEmissives = true;
+            }
+
+            if (updateEmissives)
+            {
+                CheckAndSetEmissives(true);
+            }
+        }
+
+        // Conditions and colors go here.
+        void CheckAndSetEmissives(bool _force = false)
+        {
+            foreach (var emissive in EmissiveDataSet)
+            {
+                if (Block.IsFunctional)
+                {
+                    if (Block.IsWorking)
+                    {
+                        if (EmissiveColor != emissive.FullyWorkingEmissiveColor || _force)
+                        {
+                            EmissiveColor = emissive.FullyWorkingEmissiveColor;
+                            Block.SetEmissiveParts(emissive.EmissiveMaterialName, EmissiveColor, emissive.FullyWorkingEmissiveGlow);
+                        }
+                    }
+                    else
+                    {
+                        if (EmissiveColor != emissive.BustedEmissiveColor || _force)
+                        {
+                            EmissiveColor = emissive.BustedEmissiveColor;
+                            Block.SetEmissiveParts(emissive.EmissiveMaterialName, EmissiveColor, emissive.BustedEmissiveGlow);
+                        }
+                    }
+                }
+                else
+                {
+                    if (EmissiveColor != emissive.BustedEmissiveColor || _force)
+                    {
+                        EmissiveColor = emissive.BustedEmissiveColor;
+                        Block.SetEmissiveParts(emissive.EmissiveMaterialName, EmissiveColor, emissive.BustedEmissiveGlow);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,172 @@
+﻿using Sandbox.ModAPI;
+using System.Linq;
+using System.Threading.Tasks;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Interfaces.Terminal;
+using SpaceEngineers.Game.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRageMath;
+using VRage.Utils;
+
+using PSYCHO.ApplyEmissives;
+
+/*
+    IMPORTANT!
+    Note, this only works for terminal blocks. Simply said, if a block you'd like to add emissive handling to is available in the ships terminal, you can add to it.
+*/
+
+namespace PSYCHO.ApplyEmissivesSettings
+{
+    public class Settings
+    {
+        ApplyEmissivesHandler EmissiveDataInstance => ApplyEmissivesHandler.EmissiveDataInstance;
+
+        EmissiveData emissiveData;
+
+        // ALL DATA GOES IN HERE!
+        public void ConstructData()
+        {
+            // DECLARE A NEW DATA SET
+            emissiveData = new EmissiveData();
+
+            // SET DATA
+            emissiveData.FullyWorkingEmissiveColor = new Color(0, 255, 0);
+            emissiveData.FullyWorkingEmissiveGlow = 1f;
+            emissiveData.BustedEmissiveColor = new Color(255, 0, 0);
+            emissiveData.BustedEmissiveGlow = 1f;
+
+            // ADD DATA TO BLOCKS FOR BY MATERIAL NAME
+            EmissiveDataInstance.AddData("SomeSubtypeID", "EmissiveMaterialName", emissiveData);
+            EmissiveDataInstance.AddData("SomeSubtypeID", "OtherEmissiveMaterialName", emissiveData);
+            EmissiveDataInstance.AddData("SomeOtherSubtypeID", "EmissiveMaterialName", emissiveData);
+
+            // DECLARE A NEW DATA SET
+            emissiveData = new EmissiveData();
+
+            // SET DATA
+            emissiveData.FullyWorkingEmissiveColor = new Color(0, 0, 255);
+            emissiveData.FullyWorkingEmissiveGlow = 1f;
+            emissiveData.BustedEmissiveColor = new Color(255, 0, 0);
+            emissiveData.BustedEmissiveGlow = 1f;
+
+            EmissiveDataInstance.AddData("SomeOtherOtherSubtypeID", "EmissiveMaterialName", emissiveData);
+        }
+
+
+
+
+
+
+
+
+
+        // =======================
+        // PLEASE IGNORE THE BELOW
+        // DEV LEFTOVERS
+        // =======================
+
+        /*
+        Color FullyWorkingEmissiveColor = new Color(242, 110, 80); // Emissive color when the block is working and enabled.
+        float FullyWorkingEmissiveGlow = 1f;                       // Emissive glow when the block is working and enabled.
+        Color BustedEmissiveColor = new Color(255, 0, 0);          // Emissive color when the block is broken or disabled.
+        float BustedEmissiveGlow = 1f;                             // Emissive glow when the block is broken or disabled.
+
+        string[] MaterialName =
+            { "Emissive", "Emissive2" }
+        ;
+
+        // UNCOMMENT THE BLOCK TYPES YOU WISH TO APPLY THIS SCRIPT ON
+        private readonly HashSet<MyObjectBuilderType> BlockTypes = new HashSet<MyObjectBuilderType>()
+        {
+            //typeof(MyObjectBuilder_BatteryBlock),
+            //typeof(MyObjectBuilder_ButtonPanel),
+            //typeof(MyObjectBuilder_Beacon),
+            //typeof(MyObjectBuilder_Cockpit),
+            //typeof(MyObjectBuilder_AdvancedDoor),
+            //typeof(MyObjectBuilder_AirtightHangarDoor),
+            //typeof(MyObjectBuilder_AirtightSlideDoor),
+            //typeof(MyObjectBuilder_AirVent),
+            //typeof(MyObjectBuilder_Assembler),
+            //typeof(MyObjectBuilder_CameraBlock),
+            //typeof(MyObjectBuilder_Collector),
+            //typeof(MyObjectBuilder_ContractBlock),
+            //typeof(MyObjectBuilder_Conveyor),
+            //typeof(MyObjectBuilder_ConveyorConnector),
+            //typeof(MyObjectBuilder_ConveyorSorter),
+            //typeof(MyObjectBuilder_ConveyorTurretBase),
+            //typeof(MyObjectBuilder_CryoChamber),
+            //typeof(MyObjectBuilder_Decoy),
+            //typeof(MyObjectBuilder_Door),
+            //typeof(MyObjectBuilder_Drill),
+            //typeof(MyObjectBuilder_GasTank),
+            //typeof(MyObjectBuilder_GravityGenerator),
+            //typeof(MyObjectBuilder_Gyro),
+            //typeof(MyObjectBuilder_HydrogenEngine),
+            //typeof(MyObjectBuilder_InteriorLight),
+            //typeof(MyObjectBuilder_InteriorTurret),
+            //typeof(MyObjectBuilder_Jukebox),
+            //typeof(MyObjectBuilder_JumpDrive),
+            //typeof(MyObjectBuilder_Kitchen),
+            //typeof(MyObjectBuilder_LaserAntenna),
+            //typeof(MyObjectBuilder_LargeGatlingTurret),
+            //typeof(MyObjectBuilder_LargeMissileTurret),
+            //typeof(MyObjectBuilder_LCDPanelsBlock),
+            //typeof(MyObjectBuilder_LightingBlock),
+            //typeof(MyObjectBuilder_MedicalRoom),
+            //typeof(MyObjectBuilder_MergeBlock),
+            //typeof(MyObjectBuilder_MotorAdvancedRotor),
+            //typeof(MyObjectBuilder_MotorAdvancedStator),
+            //typeof(MyObjectBuilder_MotorRotor),
+            //typeof(MyObjectBuilder_MotorStator),
+            //typeof(MyObjectBuilder_MotorSuspension),
+            //typeof(MyObjectBuilder_MyProgrammableBlock),
+            //typeof(MyObjectBuilder_OreDetector),
+            //typeof(MyObjectBuilder_OxygenFarm),
+            //typeof(MyObjectBuilder_OxygenGenerator),
+            //typeof(MyObjectBuilder_OxygenTank),
+            //typeof(MyObjectBuilder_Parachute),
+            //typeof(MyObjectBuilder_Passage),
+            //typeof(MyObjectBuilder_PistonBase),
+            //typeof(MyObjectBuilder_PistonTop),
+            //typeof(MyObjectBuilder_Planter),
+            //typeof(MyObjectBuilder_Projector),
+            //typeof(MyObjectBuilder_RadioAntenna),
+            //typeof(MyObjectBuilder_Reactor),
+            //typeof(MyObjectBuilder_Refinery),
+            //typeof(MyObjectBuilder_ReflectorLight),
+            //typeof(MyObjectBuilder_RemoteControl),
+            //typeof(MyObjectBuilder_SafeZone),
+            //typeof(MyObjectBuilder_SensorBlock),
+            //typeof(MyObjectBuilder_ShipConnector),
+            //typeof(MyObjectBuilder_ShipGrinder),
+            //typeof(MyObjectBuilder_ShipWelder),
+            //typeof(MyObjectBuilder_SignalLight),
+            //typeof(MyObjectBuilder_SmallMissileLauncher),
+            //typeof(MyObjectBuilder_SmallMissileLauncherReload),
+            //typeof(MyObjectBuilder_SolarPanel),
+            //typeof(MyObjectBuilder_SoundBlock),
+            //typeof(MyObjectBuilder_SpaceBall),
+            //typeof(MyObjectBuilder_StoreBlock),
+            //typeof(MyObjectBuilder_SurvivalKit),
+            //typeof(MyObjectBuilder_TextPanel),
+            //typeof(MyObjectBuilder_Thrust),
+            //typeof(MyObjectBuilder_TimerBlock),
+            //typeof(MyObjectBuilder_UpgradeModule),
+            //typeof(MyObjectBuilder_VendingMachine),
+            //typeof(MyObjectBuilder_VirtualMass),
+            //typeof(MyObjectBuilder_Warhead),
+            //typeof(MyObjectBuilder_Welder),
+            //typeof(MyObjectBuilder_WindTurbine),
+        };
+        */
+    }
+}
